fc957819ffb570c6986b63c30737cf528d2d0b10
songht7 <407794660@qq.com>
2020-12-27 14:03:38 +0800

    xxx

--- utils/common.js
+++ utils/common.js
@@ -6,16 +6,16 @@
  */
 var api = {
   "formal": {
-    "apiurl": "https://tmskeshun.com.cn/", //接口 https://tmskeshun.com.cn/ https://tms.keshun.com.cn
-    "pageurl": "https://tmstest.keshun.com.cn/",
+    "apiurl": "https://tmskeshun.com.cn", //接口 https://tmskeshun.com.cn/ https://tms.keshun.com.cn
+    "pageurl": "https://tmstest.keshun.com.cn",
     "imgurl": "", //图片域名https://tms.keshun.com.cn
     "domain": "",
     "appName": "CKS 科顺",
     "ai": "jjlsdjksafh-wx25f61e4ffcead9c7-wxcead9c7-wx69ecaa1a4fb809ff-213123fdsajkfhkj"
   },
   "dev": {
-    "apiurl": "https://tmstest.keshun.com.cn/", //接口 https://www.codesystem.com.cn/cks_api https://tms.keshun.com.cn
-    "pageurl": "http://tmstest.keshun.com.cn/", //业务域名
+    "apiurl": "https://tmstest.keshun.com.cn", //接口 https://www.codesystem.com.cn/cks_api https://tms.keshun.com.cn
+    "pageurl": "http://tmstest.keshun.com.cn", //业务域名
     "imgurl": "", //图片域名
     "domain": "",
     "appName": "CKS 科顺",
